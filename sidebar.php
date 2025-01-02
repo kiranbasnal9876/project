8e53331c1b557c332f02b69cdeb4b62888994a85
kiranbasnal9876 <dimpalbasnal0@gmail.com>
2025-01-02 22:42:42 +0530

    first commit from PG

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <div class="sidebar-div">
-
+<div>hi</div>
     <a href="http://localhost/First_Project/dashboard.php"><span><img src="../images/airplay.svg" alt=""></span>Dasboard</a>
     <a href="http://localhost/First_Project/usermaster/usermaster.php"><span><img src="../images/users.svg" alt=""></span>User Master</a>
     <a href="http://localhost/First_Project/clientmaster/client_master.php"><span><img src="../images/user.svg" alt=""></span>Client Master</a>
